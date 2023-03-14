1998f851ea766370ad286e334d5eca0b1a5d8cec
Michael D'Angelo <michael.l.dangelo@gmail.com>
2023-03-13 21:12:35 -0400

    use edge runtime

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export const config = {
+  runtime: 'edge',
+}
+
+export default async function (req: NextApiRequest,
+  res: NextApiResponse<Data>) {
   res.status(200).json({ name: 'John Doe' })
 }
