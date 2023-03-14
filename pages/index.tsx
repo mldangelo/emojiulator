bd7644ecf4c4f1969f72ba050b18e4f185616a96
Michael D'Angelo <michael.l.dangelo@gmail.com>
2023-03-13 22:40:48 -0400

    configure tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,28 @@
 // next js hello world page for static export 
 
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
-
-    return (<>
-    <Head>
+export default function Index() {
+    return (
+        <>
+            <Head>
         <title>Emojiulator</title>
         <meta name="description" content="Find the perfect emoji for anything" />
         <link rel="icon" href="/favicon.ico" />
     </Head>
-
-    <main>
-        <h1 className={styles.title}>
-            Welcome to Emojiulator!
-        </h1>
-    </main>
-    <footer>
-        <a
-            href="https://mldangelo.com"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            Powered by{' '}
-        </a>
-    </footer>
-    </>
-    );
-}
+      <div className="bg-gray-900 py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Emojiulator</h2>
+            <p className="mt-6 text-lg leading-8 text-gray-300">
+              Translate any word, phrase, or text into emojis.
+            </p>
+          </div>
+        </div>
+      </div>
+      </>
+    )
+  }
 
 export const config = {
     runtime: 'experimental-edge',
