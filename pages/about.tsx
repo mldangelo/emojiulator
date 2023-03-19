cfb21a51131989067841f8acb9778261c8111816
ggiiaa <88732927+ggiiaa@users.noreply.github.com>
2023-03-19 05:08:54 +0000

    add about page

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div>
+      <p>
+        Emojiulator started with a dream. What if any word, scentence, or phrase
+        could be translated into emojis?
+      </p>
+      <p>We are here to answer that dream.</p>
+      <p>Welcome. We are happy to see you here.</p>
+    </div>
+  );
+}
+
+export const config = {
+  runtime: "experimental-edge",
+};
